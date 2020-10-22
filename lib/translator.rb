ff4068da9caefb2726bf04867f385e68a80e0da1
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 16:46:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,18 +5,32 @@ require 'yaml'
 
 def load_library(file_path)
 
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  # emoticons = YAML.load_file('./lib/emoticons.yml')
+  #
+  # emoticon_hashes = {}
+  #
+  # emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+  #
+  #   english = value[0]
+  #   japanese = value[1]
+  #   emoticon_hashes[meaning] = emoticon_hashes[meaning]
+  #   emoticon-translator = {"english" => english, "japanese" => japanese}
+  #   binding.pry
+  # end
+  emoticons = YAML.load_file(path)
+  lookup = {}
+  lookup[:get_meaning] = {}
+  lookup[:get_emoticon] = {}
 
-  emoticon_hashes = {}
+  emoticons.each do |word, pair|
+    japanese = pair[1]
+    american = pair[0]
 
-  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-
-    english = value[0]
-    japanese = value[1]
-    # emoticon_hashes[meaning] = emoticon_hashes[meaning]
-    emoticon-translator = {"english" => english, "japanese" => japanese}
-    binding.pry
+    lookup[:get_meaning][japanese] = word
+    lookup[:get_emoticon][american] = japanese
   end
+
+  lookup
 end
 
 def get_japanese_emoticon
