b9abb539362e8d6044b92113ca692121858a4642
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 16:12:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,11 +9,10 @@ def load_library(file_path)
 
   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
 
-    emoticon-meaning = {}
-    emoticon-translator = {}
+    emoticon-meaning = {meaning}
+    english = value[0]
+    japanese = value[1]
     binding.pry
-    # english = value[0]
-    # japanese = value[1]
     # emoticon_lib['get_meaning'][japanese] = meaning
     # emoticon_lib['get_emoticon'][english] = japanese
   end
