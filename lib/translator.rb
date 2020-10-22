db8322569a39c9503ba1d0b7a451aab67c4c48e7
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 16:58:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,6 @@
-# require modules here
 require 'pry'
 require 'yaml'
-
 
 def load_library(file_path)
 
   emoticons = YAML.load_file('./lib/emoticons.yml')
-
-  emoticon_hashes = {}
-
-  emoticons.each do |key, value|
-    emoticon_hashes[key] = {}
-    emoticon_hashes[key][:english] = value[0]
-    emoticon_hashes[key][:japanese] = value[1]
-  end
-  emoticon_hashes
-  binding.pry
-end
