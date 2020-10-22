5718f9d8c7a5b45f50b732fb13b788b3631d90ea
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 15:47:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,9 +15,9 @@ def load_library(file_path)
     japanese = value[1]
     emoticon_lib['get_meaning'][japanese] = meaning
     emoticon_lib['get_emoticon'][english] = japanese
+    binding.pry
   end
   emoticon_lib
-  binding.pry
 end
 
 def get_japanese_emoticon
