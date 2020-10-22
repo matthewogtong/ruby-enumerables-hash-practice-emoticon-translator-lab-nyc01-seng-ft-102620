2b2743822e9150d485ae639b6f28d7784ca9d765
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 16:53:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,14 +7,21 @@ def load_library(file_path)
 
   emoticons = YAML.load_file('./lib/emoticons.yml')
 
-  emoticon_hashes = {}
-
-  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-
-    english = value[0]
-    japanese = value[1]
-    emoticon_hashes[meaning] = emoticon_hashes[meaning]
-    binding.pry
-    emoticon-translator = {"english" => english, "japanese" => japanese}
+  # emoticon_hashes = {}
+  #
+  # emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+  #
+  #   english = value[0]
+  #   japanese = value[1]
+  #   emoticon_hashes[meaning] = emoticon_hashes[meaning]
+  #   binding.pry
+  #   emoticon-translator = {"english" => english, "japanese" => japanese}
+  # end
+  empty_hash = {}
+  emoticons.each do |key, value|
+    empty_hash[key] = {}
+    empty_hash[key][:english] = value[0]
+    empty_hash[key][:japanese] = value[1]
   end
+  empty_hash
 end
