4e2c648369cb261fbb1a4584d57f857e5f9f21fe
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 16:15:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,10 +8,10 @@ def load_library(file_path)
   emoticons = YAML.load_file('./lib/emoticons.yml')
 
   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-    
+
     english = value[0]
     japanese = value[1]
-    emoticon-meaning = 
+    emoticon-meaning =
     {
       meaning => {
         :english => english,
