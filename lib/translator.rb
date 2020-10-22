834ed1e7c6e88f00ae742230c60cc9b92664fc97
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 17:22:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(file_path, emoticon)
 
   translated_emoticons.each do |meaning, value|
     if translated_emoticons[meaning][:japanese] == emoticon
-      english_meaning = translated_emoticons[meaning][:english]
+      return key
     end
     binding.pry
     english_meaning
