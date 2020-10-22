9881c1e18018c602b89f6f7a455d939e481a9255
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 15:42:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,9 +9,9 @@ def load_library(file_path)
   library = YAML.load_file(yaml_file)
   library.each do |trans, emos|
     response["get_meaning"][emos[1]] = trans
+    binding.pry
     response["get_emoticon"][emos[0]] = emos[1]
   end
-  binding.pry
   response
 end
 
