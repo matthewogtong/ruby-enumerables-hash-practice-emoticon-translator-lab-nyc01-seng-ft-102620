86959d6241aae84d9a82a3bf755a53b99f920e93
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 15:07:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,27 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+
+  emoticon_library = YAML.load_file(file_path)
+
+
+  emoticon_library.each do |emote_name, emote_value|
+
+    emote_value.each do |emote|
+      if !emote_name = {emote_name => {:english => emote}}
+        {emote_name => {:english => emote}}
+      elsif !emote_name = {emote_name => {:english => emote}} && !emote_name = {emote_name => {:japanese => emote}}
+        {emote_name => {:japanese => emote}}
+      end
+      # if !emote_name = {:english => emote} && !emote_name = {:japanese => emote}
+      #   emote_name = {"japanese": emote}
+      # end
+      binding.pry
+    end
+  end
 end
 
 def get_japanese_emoticon
